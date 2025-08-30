b74925c011ee16d3bc9f5b380c21b3bcd9f2ed9f
CriFigShe <crifigshe@gmail.com>
2025-08-30 12:09:34 +0200

    Image service implementation

---
+++ src/main/java/com/cristian/dream_shops/repository/ImageRepository.java
@@ -0,0 +1,8 @@
+package com.cristian.dream_shops.repository;
+
+import com.cristian.dream_shops.model.Image;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ImageRepository extends JpaRepository<Image, Long> {
+
+}
